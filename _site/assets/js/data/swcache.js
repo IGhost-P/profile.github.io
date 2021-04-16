315dbdc83960803149242ac6b509d7d155542ed2
IGhost-P <79236624+IGhost-P@users.noreply.github.com>
2021-04-16 19:53:24 +0900

    번들 적용

--- _site/assets/js/data/swcache.js
+++ _site/assets/js/data/swcache.js
@@ -1,1 +1,68 @@
-const resource = [ /* --- CSS --- */ '/profile.github.io/assets/css/style.css', /* --- JavaScripts --- */ '/profile.github.io/assets/js/dist/home.min.js', '/profile.github.io/assets/js/dist/page.min.js', '/profile.github.io/assets/js/dist/post.min.js', '/profile.github.io/assets/js/dist/categories.min.js', '/profile.github.io/assets/js/data/search.json', '/profile.github.io/app.js', '/profile.github.io/sw.js', /* --- HTML --- */ '/profile.github.io/index.html', '/profile.github.io/404.html', /* --- Icons --- */ '/profile.github.io/assets/img/favicons/favicon.ico', '/profile.github.io/assets/img/favicons/apple-icon.png', '/profile.github.io/assets/img/favicons/apple-icon-precomposed.png', '/profile.github.io/assets/img/favicons/apple-icon-57x57.png', '/profile.github.io/assets/img/favicons/apple-icon-60x60.png', '/profile.github.io/assets/img/favicons/apple-icon-72x72.png', '/profile.github.io/assets/img/favicons/apple-icon-76x76.png', '/profile.github.io/assets/img/favicons/apple-icon-114x114.png', '/profile.github.io/assets/img/favicons/apple-icon-120x120.png', '/profile.github.io/assets/img/favicons/apple-icon-144x144.png', '/profile.github.io/assets/img/favicons/apple-icon-152x152.png', '/profile.github.io/assets/img/favicons/apple-icon-180x180.png', '/profile.github.io/assets/img/favicons/android-icon-192x192.png', '/profile.github.io/assets/img/favicons/favicon-32x32.png', '/profile.github.io/assets/img/favicons/favicon-96x96.png', '/profile.github.io/assets/img/favicons/favicon-16x16.png', '/profile.github.io/assets/img/favicons/ms-icon-144x144.png', '/profile.github.io/assets/img/favicons/manifest.json', '/profile.github.io/assets/img/favicons/browserconfig.xml' ]; /* The request url with below domain will be cached */ const allowedDomains = [ 'www.googletagmanager.com', 'www.google-analytics.com', 'github.com/IGhost-P/profile.github.io', 'fonts.gstatic.com', 'fonts.googleapis.com', 'cdn.jsdelivr.net', 'polyfill.io' ]; /* Requests that include the following path will be banned */ const denyUrls = [ ];
+const resource = [
+
+  /* --- CSS --- */
+  '/assets/css/style.css',
+
+  /* --- JavaScripts --- */
+  
+  '/assets/js/dist/home.min.js',
+  '/assets/js/dist/page.min.js',
+  '/assets/js/dist/post.min.js',
+  '/assets/js/dist/categories.min.js',
+  '/assets/js/data/search.json',
+  '/app.js',
+  '/sw.js',
+
+  /* --- HTML --- */
+  '/index.html',
+  '/404.html',
+  
+    '/categories/',
+  
+    '/tags/',
+  
+    '/archives/',
+  
+    '/about/',
+  
+
+  /* --- Icons --- */
+  
+  '/assets/img/favicons/favicon.ico',
+  '/assets/img/favicons/apple-icon.png',
+  '/assets/img/favicons/apple-icon-precomposed.png',
+  '/assets/img/favicons/apple-icon-57x57.png',
+  '/assets/img/favicons/apple-icon-60x60.png',
+  '/assets/img/favicons/apple-icon-72x72.png',
+  '/assets/img/favicons/apple-icon-76x76.png',
+  '/assets/img/favicons/apple-icon-114x114.png',
+  '/assets/img/favicons/apple-icon-120x120.png',
+  '/assets/img/favicons/apple-icon-144x144.png',
+  '/assets/img/favicons/apple-icon-152x152.png',
+  '/assets/img/favicons/apple-icon-180x180.png',
+  '/assets/img/favicons/android-icon-192x192.png',
+  '/assets/img/favicons/favicon-32x32.png',
+  '/assets/img/favicons/favicon-96x96.png',
+  '/assets/img/favicons/favicon-16x16.png',
+  '/assets/img/favicons/ms-icon-144x144.png',
+  '/assets/img/favicons/manifest.json',
+  '/assets/img/favicons/browserconfig.xml'
+];
+
+/* The request url with below domain will be cached */
+const allowedDomains = [
+  
+
+  '',
+
+  'fonts.gstatic.com',
+  'fonts.googleapis.com',
+  'cdn.jsdelivr.net',
+  'polyfill.io'
+];
+
+/* Requests that include the following path will be banned */
+const denyUrls = [
+  
+];
+
